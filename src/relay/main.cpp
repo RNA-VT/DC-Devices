05c2bb4e88f66943c0cc79fee85c564990f4a681
Olin <olin@olins-mbp.lan>
2023-10-03 16:04:23 -0600

    use old wifimanager wrapper, we work client to be a bit more resilient

---
+++ src/relay/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <WiFiManagerWrapper.h>
+#include <ESP8266WiFi.h>
+
+WiFiManagerWrapper wfm = WiFiManagerWrapper();
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  wifi_set_sleep_type(NONE_SLEEP_T);
+
+  // wfm.add_param("device_name", "Device Name", "device-01", 60);
+
+  wfm.setup_wifi_manager();
+
+  // Serial.println(wfm.getParamValue("device_name"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello World!");
+
+  wfm.do_loop();
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
